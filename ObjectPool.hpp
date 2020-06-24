385de45d2b93995be4aacaebefe145a51bf3ab47
YTX\yuanbo.pan <yuanbo.pan@yintech.cn>
2020-06-24 20:02:41 +0800

    object pool

--- ObjectPool.hpp
+++ ObjectPool.hpp
@@ -23,7 +23,9 @@ public:
 		auto constructName = typeid(Constructor<Args...>).name(); //不区分引用
 		for (size_t i = 0; i <num; i++)
 		{
-			m_object_map.emplace(constructName, shared_ptr<T>(new T(std::forward<Args>(args)...), [this, constructName](T* p) //删除器中不直接删除对象，而是回收到对象池中，以供下次使用
+			m_object_map.emplace(constructName, \
+				shared_ptr<T>(new T(std::forward<Args>(args)...), \
+					[this, constructName](T* p) //删除器中不直接删除对象，而是回收到对象池中，以供下次使用
 			{
 				m_object_map.emplace(std::move(constructName), std::shared_ptr<T>(p));
 			}));
@@ -35,6 +37,8 @@ public:
 	std::shared_ptr<T> Get()
 	{
 		string constructName = typeid(Constructor<Args...>).name();
+
+		//std::cout << "constructName:\t" << constructName.c_str() << std::endl;
 
 		auto range = m_object_map.equal_range(constructName);
 		for (auto it = range.first; it != range.second; ++it)
--- da_second.cpp
+++ da_second.cpp
@@ -3,6 +3,7 @@
 #include <set>
 #include <map>
 #include <vector>
+#include "ObjectPool.hpp"
 
 using namespace std;
 
@@ -23,13 +24,53 @@ int f()
 }
 
 
+class Player {
+public:
+	Player(int age) { age_ = age; }
+
+	int age_;
+};
+
+struct MyObject {
+	MyObject() 
+	{ 
+		cout << "MyObject " << endl;
+	}
+	MyObject(int a) { cout << "MyObject int" << endl; }
+
+	MyObject(const int& a, const int& b) { cout << "MyObject int int" << endl; }
+
+	void display(const string& s) {
+		cout << "display:" << s.c_str() << endl;
+	}
+};
+
+void display(shared_ptr<MyObject> sp, const string& s) 
+{
+	if (nullptr != sp)
+	{
+		sp->display(s);
+	}
+}
+
+void TestObjPool() 
+{
+	ObjectPool<MyObject> pool;
+	pool.Init(3);
+	{
+		auto p = pool.Get();
+		display(p, "p");
+		auto p2 = pool.Get();
+		display(p2, "p2");
+	}
+
+	auto p = pool.Get();
+	display(p, "p");
+}
 
 int main()
 {
-
-	std::cout << "C::f() = " << C::f() << ','
-		<< " ::f() = " << ::f() << '\n';
-
+	TestObjPool();
 
 	return 0;
 }
